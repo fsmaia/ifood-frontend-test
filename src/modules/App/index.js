0891d24372701ddb249b8a208b4f20829a2e10f4
Fernando Maia <fsmaia@gmail.com>
2019-01-20 00:23:04 -0200

    Modularize App and apply BEM

--- src/modules/App/index.js
+++ src/modules/App/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.scss';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <header className="App__header">
+      <img src={logo} className="App__logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <a className="App__link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
     </header>
