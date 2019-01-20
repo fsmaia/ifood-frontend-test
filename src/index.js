b7df72a8b1f953000809498348896700d3dc6a88
Fernando Maia <fsmaia@gmail.com>
2019-01-20 16:05:50 -0200

    Add react-router setup and apply redux-persist in application

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './modules/App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore';
 
-const store = configureStore(window.__REDUX_STATE__ || {});
+const { store, persistor } = configureStore(window.__REDUX_STATE__ || {});
 
 ReactDOM.render(
   <ReduxProvider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </ReduxProvider>,
   document.getElementById('root')
 );
