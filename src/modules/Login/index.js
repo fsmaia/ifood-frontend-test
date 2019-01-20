184458547941e04bf7fa0f34f904d55cfda48a05
Fernando Maia <fsmaia@gmail.com>
2019-01-20 17:25:17 -0200

    Implement Login layout

--- src/modules/Login/index.js
+++ src/modules/Login/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { SPOTIFY_AUTH_REDIRECT_URL } from '../../constants/spotify';
+import './index.scss';
+import Button from '../UI/components/Button';
 
 const onLoginClick = () => {
   window.location.href = SPOTIFY_AUTH_REDIRECT_URL;
@@ -9,9 +11,19 @@ const onLoginClick = () => {
 
 const Login = ({ className }) => (
   <div className={classNames(className, 'Login')}>
-    <button type="button" onClick={onLoginClick}>
-      Login
-    </button>
+    <h1 className="Login__title">Welcome to Spotifood.</h1>
+
+    <p className="Login__subtitle">
+      This is where food and music universes meet. Begin your journey connecting to Spotify by using
+      the button below.
+    </p>
+
+    <Button
+      className="Login__button"
+      icon="/spotify-inverted.png"
+      text="Connect with Spotify"
+      onClick={onLoginClick}
+    />
   </div>
 );
 
