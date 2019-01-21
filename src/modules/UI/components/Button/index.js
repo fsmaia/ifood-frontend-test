486656f9cd5627fcae7b7760a910001bc03b92c5
Fernando Maia <fsmaia@gmail.com>
2019-01-20 23:45:55 -0200

    Add  modifier to

--- src/modules/UI/components/Button/index.js
+++ src/modules/UI/components/Button/index.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './index.scss';
 
-const Button = ({ className, icon, text, type, ...props }) => (
-  <button className={classNames(className, 'Button')} type={type} {...props}>
+const Button = ({ className, icon, secondary, text, type, ...props }) => (
+  <button
+    className={classNames(className, 'Button', secondary && 'Button--secondary')}
+    type={type}
+    {...props}
+  >
     {icon && <img className="Button__icon" src={icon} alt={text} />}
     <span className="Button__text">{text}</span>
   </button>
@@ -12,11 +16,16 @@ const Button = ({ className, icon, text, type, ...props }) => (
 
 Button.propTypes = {
   className: PropTypes.string,
+  icon: PropTypes.string,
+  secondary: PropTypes.bool,
+  text: PropTypes.string.isRequired,
   type: PropTypes.string
 };
 
 Button.defaultProps = {
   className: '',
+  icon: null,
+  secondary: false,
   type: 'button'
 };
 
